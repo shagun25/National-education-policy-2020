b56a5fa9705e5a925c7fd3d247525f1423efc740
CHARU SACHDEVA <60181628+Charu271@users.noreply.github.com>
2020-09-17 19:00:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,68 @@
-window.addEventListener('scroll', function () {
-    const scrolled = window.scrollY;
-    console.log(scrolled);
-    if(scrolled>1)
-    {
-      document.getElementById("navbar").style.backgroundColor="rgb(233, 230, 230)";
+var TxtType = function (el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = "";
+  this.tick();
+  this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function () {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+
+  if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>";
+
+  var that = this;
+  var delta = 200 - Math.random() * 100;
+
+  if (this.isDeleting) {
+    delta /= 2;
+  }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === "") {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+  }
+
+  setTimeout(function () {
+    that.tick();
+  }, delta);
+};
+
+window.onload = function () {
+  var elements = document.getElementsByClassName("typewrite");
+  for (var i = 0; i < elements.length; i++) {
+    var toRotate = elements[i].getAttribute("data-type");
+    var period = elements[i].getAttribute("data-period");
+    if (toRotate) {
+      new TxtType(elements[i], JSON.parse(toRotate), period);
     }
-    if(scrolled==0)
-    {
-      document.getElementById("navbar").style.backgroundColor="transparent";
-    }
-    
-  });
+  }
+  // INJECT CSS
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+  document.body.appendChild(css);
+};
+window.addEventListener("scroll", () => {
+  const scrolled = window.scrollY;
+  console.log(scrolled);
+  if (scrolled > 1) {
+    document.getElementById("navbar").style.backgroundColor = "";
+  }
+  if (scrolled == 0) {
+    document.getElementById("navbar").style.backgroundColor = "transparent";
+  }
+});
